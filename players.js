e62109c52c85a1eb1302ac131a77428ae6f538fe
Shohag <shohag.programmer@gmail.com>
2021-03-01 10:38:49 +0600

    new fill added

---
+++ players.js
@@ -0,0 +1,29 @@
+[{"key":"BDP001","category":"cricketer","name":"Anamul","age":"31","salary":"500","address":"dhaka","img":"https://raw.githubusercontent.com/shohagpro/host/main/anamul.jpg","url":"https://raw.githubusercontent.com/shohagpro/host/main/anamul.jpg","feature":"bowler","height":5.67},
+	
+	,{"key":"BDP002","category":"cricketer","name":"Liton","age":"29","salary":"400","address":"gazipur","img":"https://raw.githubusercontent.com/shohagpro/host/main/litondas.jpg","url":"https://raw.githubusercontent.com/shohagpro/host/main/litondas.jpg","feature":"bowler","height":6.12},
+	
+	,{"key":"BDP003","category":"cricketer","name":"Mahmudullah","age":"33","salary":"600","address":"chittagong","img":"https://raw.githubusercontent.com/shohagpro/host/main/mahmudullah.jpg","url":"https://raw.githubusercontent.com/shohagpro/host/main/mahmudullah.jpg","feature":"batsman","height":6.16},
+	
+	,{"key":"BDP004","category":"cricketer","name":"Mashrafi","age":"39","salary":"700","address":"norail","img":"https://raw.githubusercontent.com/shohagpro/host/main/mashrafi.jpg","url":"https://raw.githubusercontent.com/shohagpro/host/main/mashrafi.jpg","feature":"bowler","height":6.56},
+	
+	,{"key":"BDP005","category":"cricketer","name":"Mehedi","age":"21","salary":"200","address":"rangpur","img":"https://raw.githubusercontent.com/shohagpro/host/main/mehedi.jpg","url":"https://raw.githubusercontent.com/shohagpro/host/main/mehedi.jpg","feature":"bowler","height":5.52},
+	
+	,{"key":"BDP006","category":"cricketer","name":"Mosaddek","age":"24","salary":"300","address":"sylhet","img":"https://raw.githubusercontent.com/shohagpro/host/main/mosaddek.jpg","url":"https://raw.githubusercontent.com/shohagpro/host/main/mosaddek.jpg","feature":"bowler","height":5.72},
+	
+	,{"key":"BDP007","category":"cricketer","name":"Mushfiq","age":"33","salary":"600","address":"khulna","img":"https://raw.githubusercontent.com/shohagpro/host/main/mushfiq.jpg","url":"https://raw.githubusercontent.com/shohagpro/host/main/mushfiq.jpg","feature":"keeper","height":5.22},
+	
+	,{"key":"BDP008","category":"cricketer","name":"Rubel","age":"27","salary":"450","address":"comilla","img":"https://raw.githubusercontent.com/shohagpro/host/main/rubel.jpg","url":"https://raw.githubusercontent.com/shohagpro/host/main/rubel.jpg","feature":"bowler","height":5.89},
+	
+	,{"key":"BDP009","category":"cricketer","name":"Sabbir","age":"26","salary":"430","address":"barisal","img":"https://raw.githubusercontent.com/shohagpro/host/main/sabbir.jpg","url":"https://raw.githubusercontent.com/shohagpro/host/main/sabbir.jpg","feature":"bowler","height":5.62},
+	
+	,{"key":"BDP010","category":"cricketer","name":"Saifuddin","age":"22","salary":"390","address":"jamalpur","img":"https://raw.githubusercontent.com/shohagpro/host/main/saifuddin.jpg","url":"https://raw.githubusercontent.com/shohagpro/host/main/saifuddin.jpg","feature":"bowler","height":6.32},
+	
+	,{"key":"BDP011","category":"cricketer","name":"Shakib","age":"30","salary":"900","address":"dhaka","img":"https://raw.githubusercontent.com/shohagpro/host/main/sakib.jpg","url":"https://raw.githubusercontent.com/shohagpro/host/main/sakib.jpg","feature":"allrounder","height":6.22},
+	
+	,{"key":"BDP012","category":"cricketer","name":"Soumya","age":"26","salary":"500","address":"comilla","img":"https://raw.githubusercontent.com/shohagpro/host/main/soumya.jpg","url":"https://raw.githubusercontent.com/shohagpro/host/main/soumya.jpg","feature":"batsman","height":5.89},
+	
+	,{"key":"BDP013","category":"cricketer","name":"Tamim","age":"33","salary":"850","address":"chittagong","img":"https://raw.githubusercontent.com/shohagpro/host/main/tamim.jpg","url":"https://raw.githubusercontent.com/shohagpro/host/main/tamim.jpg","feature":"batsman","height":6.42},
+	
+	,{"key":"BDP014","category":"cricketer","name":"Taskin","age":"28","salary":"560","address":"dhaka","img":"https://raw.githubusercontent.com/shohagpro/host/main/taskin.jpg","url":"https://raw.githubusercontent.com/shohagpro/host/main/tskin.jpg","feature":"bowler","height":6.69},
+	
+	,{"key":"BDP015","category":"cricketer","name":"Mustafiz","age":"21","salary":"650","address":"satkhira","img":"https://raw.githubusercontent.com/shohagpro/host/main/mustafiz.jpg","url":"https://raw.githubusercontent.com/shohagpro/host/main/mustafiz.jpg","feature":"bowler","height":6.11}]
